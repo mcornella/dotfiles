11dc6e38ca557f91253db1394f6b385538358301
Marc Cornellà <marc.cornella@live.com>
2014-02-26 02:49:46 +0100

    Renamed dotfiles and updated script for simplicity

--- init.zsh
+++ init.zsh
@@ -2,10 +2,13 @@
 #
 # Symlinks files in this directory into $HOME directory
 
+# Base dir
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-SYMLINKS=(aliases dircolors functions gitconfig toprc zshrc)
 
-for file in $SYMLINKS; do
+# Files to be symlinked to home directory
+dotfiles=(.aliases .dircolors .functions .gitconfig .toprc .zshrc)
+
+for file in $dotfiles; do
     echo -n "symlinking $file... "
-    ln -s $DIR/$file $HOME/.$file >/dev/null 2>&1 && echo "[ok]" || echo "[error]"
+    ln -s $DIR/$file $HOME/$file >/dev/null 2>&1 && echo "[ok]" || echo "[error]"
 done
